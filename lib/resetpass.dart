68482488aeee4202b746b10636f73e7e261dea1f
YASHY2K <65499486+YASHY2K@users.noreply.github.com>
2021-05-17 17:37:17 +0530

    Add files via upload

---
+++ lib/resetpass.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'colorcheme.dart';
+
+class ResetScreen extends StatefulWidget {
+  @override
+  _ResetScreenState createState() => _ResetScreenState();
+}
+
+class _ResetScreenState extends State<ResetScreen> {
+  String _email;
+  final auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reset Password'),
+        backgroundColor: purple,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(hintText: 'Email'),
+              onChanged: (value) {
+                setState(() {
+                  _email = value.trim();
+                });
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              RaisedButton(
+                child: Text('Send Request',
+    style: TextStyle(color: Colors.white),),
+                onPressed: () {
+                  if (_email == null) {
+                    Fluttertoast.showToast(
+                        msg: "Please enter a valid email",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM,
+                        timeInSecForIosWeb: 1,
+                        fontSize: 16.0);
+                  } else {
+                    Fluttertoast.showToast(
+                        msg: "Reset Link Sent",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM,
+                        timeInSecForIosWeb: 1,
+                        fontSize: 16.0);
+                    auth.sendPasswordResetEmail(email: _email);
+                    Navigator.of(context).pop();
+                  }
+                },
+                color: purple,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
